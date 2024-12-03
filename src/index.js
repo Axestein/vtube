979853914d858a65001735f70d5db8e5af3a805c
Aditya Kumar Singh <adityandmb@gmail.com>
2024-12-04 01:15:34 +0530

    feat: add middleware

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ dotenv.config({
     path: './.env'
 })
 
-
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 /*
@@ -16,7 +23,7 @@ const app = express()
 ( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
+        app.on("error", (error) => {
             console.log("ERRR: ", error);
             throw error
         })
